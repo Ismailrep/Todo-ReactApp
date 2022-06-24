7877c13749049b74cc740250306712655f91c0ab
Ismail <ismaildwiprakosa@gmail.com>
2022-06-24 07:32:11 +0700

    add json

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router jsonServer.router('./db.json');
-const middlewares jsonServer.defaults({
+const router = jsonServer.router('./db.json');
+const middlewares = jsonServer.defaults({
   static: './build'
 });
 const PORT-process.env.PORT || 8000;
